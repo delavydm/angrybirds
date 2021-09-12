f4aa0a460117b2fd622437f9e52ae9f7e56c7cbb
Delavy Duque <delavyduque@hotmail.com>
2021-09-12 17:15:45 -0500

    corregido slingshot

--- slingshot.js
+++ slingshot.js
@@ -1,4 +1,4 @@
-class Slingshot{
+class SlingShot{
     constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
@@ -6,21 +6,22 @@ class Slingshot{
             stiffness: 0.04,
             length: 10
         }
+        this.pointB = pointB
         this.sling = Constraint.create(options);
-        this.pointB = pointB;
         World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
     }
 
     display(){
         if(this.sling.bodyA){
-        var pointA = this.sling.bodyA.position;
-        var pointB = this.pointB;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
     
-    fly(){
-        this.sling.bodyA=null;
-    }
 }
